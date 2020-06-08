d8afaef5cba9fabb66e0ffa4e7cd726e25b63640
Eric Gauzens <ericgauzens@hotmail.com>
2020-06-08 18:39:16 -0400

    Added billing and client side app!

--- services/passport.js
+++ services/passport.js
@@ -9,10 +9,9 @@ passport.serializeUser((user, done) => {
 	done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-	User.findById(id).then((user) => {
-		done(null, user);
-	});
+passport.deserializeUser(async (id, done) => {
+	const user = await User.findById(id);
+	done(null, user);
 });
 
 passport.use(
@@ -23,16 +22,15 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true,
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then((user) => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				done(null, existingUser);
+			} else {
+				const newUser = new User({ googleId: profile.id });
+				await newUser.save();
+				done(null, newUser);
+			}
 		}
 	)
 );
